fbb027ebfea9cae2bf9aa3d1a1d8fb1a34e266a0
Guram Kveniashvili <91364481+Kveno999@users.noreply.github.com>
2023-10-21 13:51:41 +0400

    Update PostController.js

--- blog-api/src/controllers/PostController.js
+++ blog-api/src/controllers/PostController.js
@@ -4,6 +4,9 @@ exports.createPost = async (req, res) => {
   try {
     const {title, description, categoryId} = req.body
     const categoryModel = await Category.findOne(categoryId)
+    if (categoryModel == null) {
+      return res.status(400).send({ msg: "Given Category doesn't exists" });
+    }
     const post = await Post.create({ title, description, categoryModel });
     return res.send(post);
   } catch (_) {
